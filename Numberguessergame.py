b53ca7b0b1d260d548cfdd7e441175ec95bb8453
Ramsay Domloge <rdomloge@gmail.com>
2022-09-19 20:58:08 +0100

    Added how many tries it took to complete game

--- Numberguessergame.py
+++ Numberguessergame.py
@@ -1,29 +1,20 @@
 from random import randint
 
-
-
-
 number = randint(0,10)
 stilltrying = True 
-
+count = 0
 print("Welcome to Number guesser, guess the number I have generated")
 
 while stilltrying:
 
     guessString = input("Guess a number between 0 and 10:")
     answer = int(guessString)
-    if answer>10:
-        print("What part of between 0 and 10 do you not understand, not funny+didn't laugh")
-        exit(1)
-    elif answer<0:
-        print("Why you type your braincell count")
-    else:
-        print("Good choice I guess")
+    count = count+1
 
-
-
+    print("Attempt ",count,"") 
     if answer == number: 
         print("Well done, you guessed my number")
+        print("Well done, you did this in ",count," tries")
         stilltrying = False 
     elif answer>number:
         print("That number is too high, try again")
