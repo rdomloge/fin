e42c711e6065859623797b6b334baa56f341de9c
Ramsay Domloge <rdomloge@gmail.com>
2022-09-19 20:58:31 +0100

    first draft

---
+++ Python test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+userinput = input("How old are you?") 
+
+age = int(userinput)
+
+if (age > 12):
+    print("Lol you're older than me imagine")
+
+elif (age < 12):
+    print("Obviously 0 braincells")
+
+else:
+    print("Giga Chad")
+ 
